bb8c904c17865529f8eeef60d8761785ef794433
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-13 10:32:45 -0600

    grid init

--- starwars/src/components/StarsWarsGrid.js
+++ starwars/src/components/StarsWarsGrid.js
@@ -1,2 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+
+function StarwarsGrid()
+{
+    return (
+        <div className="container">
+            <div className='enterFriends'>
+                
+            </div>
+        </div>
+    );
+}
+
+export default StarwarsGrid;
