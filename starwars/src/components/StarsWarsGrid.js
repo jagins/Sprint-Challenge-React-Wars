105471d02882da6d13bbb03a46f8588246427a44
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-13 10:45:03 -0600

    axios call

--- starwars/src/components/StarsWarsGrid.js
+++ starwars/src/components/StarsWarsGrid.js
@@ -1,15 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-function StarwarsGrid()
+export default function StarwarsGrid()
 {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() =>
+    {
+        axios.get(`https://swapi.co/api/people`)
+        .then(response =>
+        {
+            console.log(response);
+        })
+        .catch(err =>
+        {
+            console.log("OOPS not using the force", err);
+        });
+    }, []);
     return (
         <div className="container">
             <div className='enterFriends'>
-                
+
             </div>
         </div>
-    );
+    )
 }
-
-export default StarwarsGrid;
